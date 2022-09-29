5782cf359eb25bf76694d480476dce25a64daf79
Moi <ewencoquio2389@gmail.com>
2022-09-29 16:03:27 +0200

    t

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //to
-    }
+        ArrayList mul3 = new ArrayList();
+        String list;
+        for(int i =0;i<100;i++){
+            if(i%3==0){
+                mul3.add(i);
+            }
+        }
+        System.out.println("Liste des multiples de 3 : " + mul3);
 
+        list = mul3.toString();
+        System.out.println("Liste des multiples de 3 : " + list);
+    }
 }
+
+
